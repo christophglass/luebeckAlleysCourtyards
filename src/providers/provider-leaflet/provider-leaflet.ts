044b1ee4f1f76422817bb3cff5fcac4adbb14004
Christoph Glaß <mail.kaffeeserver@googlemail.com>
2018-12-09 19:56:29 +0100

    bounds

--- src/providers/provider-leaflet/provider-leaflet.ts
+++ src/providers/provider-leaflet/provider-leaflet.ts
@@ -1,5 +1,4 @@
 import { IPointOfInterest } from './../providers-gaenge/providers-gaenge';
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import leaflet from 'leaflet';
 
@@ -14,14 +13,18 @@ const tileLayer: string = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const attributions: string = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
 const maxZoom: number = 18;
 const minZoom: number = 14;
+const centerLat: number = 53.8654673;
+const centerLang: number = 10.6865593;
+const bounds = leaflet.latLng(centerLat, centerLang).toBounds(2500);
 
 @Injectable()
 export class ProviderLeafletProvider {
-  constructor(public http: HttpClient) { }
+
+  constructor() { }
 
   public GetInitialMap(): any {
     const map = this._getMap('map');
-    map.setView([53.867698, 10.685933], minZoom);
+    map.setView([centerLat, centerLang], minZoom);
     return map;
   }
 
@@ -32,12 +35,14 @@ export class ProviderLeafletProvider {
   }
 
   private _getMap(mapId: string): any {
-    const map = leaflet.map(mapId).fitWorld();
+    const map = leaflet.map(mapId).fitWorld().zoomIn();
     leaflet.tileLayer(tileLayer, {
       attributions: attributions,
       maxZoom: maxZoom,
       minZoom: minZoom,
     }).addTo(map);
+    map.setMaxBounds(bounds);
+    map.on('resize', () => { map.fitWorld(); });
     return map;
   }
 }
